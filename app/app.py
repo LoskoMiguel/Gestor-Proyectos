699ecd0214ce06d56eb95d57e48f94a546d17b7c
LoskoMiguel <magelposada@gmail.com>
2024-08-31 20:35:20 -0500

    Agregue la funcionalidad de registrar a alguien y corregi errores de el inicio de sesion

--- app/app.py
+++ app/app.py
@@ -1,33 +1,32 @@
 import json
 from flask import Flask, render_template, request, redirect
 
-# filename = "datos"
+filename = "datos.json"
 
-# try:
-#     with open(filename, "r") as file:
-#         data = json.load(file)
-#         administrador_base = data.get("administrador", [])
-#         gp_base = data.get("gestor de proyectos", [])
-#         empleados_base = data.get("empleados", [])
-#         proyectos_base = data.get("proyectos", {})
+def cargar_datos():
+    try:
+        with open(filename, "r") as file:
+            data = json.load(file)
+            administrador_base = data.get("administrador", [])
+            gp_base = data.get("gestor de proyectos", [])
+            empleados_base = data.get("empleados", [])
+            proyectos_base = data.get("proyectos", {})
+    except Exception as e:
+        administrador_base = []
+        gp_base = []
+        empleados_base = []
+        proyectos_base = {}
+
+    return administrador_base, gp_base, empleados_base, proyectos_base
+
+def guardar_datos(administrador_base, gp_base, empleados_base):
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump({"administrador": administrador_base, "gestor de proyectos": gp_base, "empleados": empleados_base}, file, indent=5)
 
 
-# except Exception as e:
-#     administrador_base = []
-#     gp_base = []
-#     empleados_base = []
+administrador_base, gp_base, empleados_base, proyectos_base = cargar_datos()
 
-administrador = [{
-    "usuario" : "losko",
-    "contraseña" : "losko1122"
-}]
-
-gp = [{
-    "usuario" : "carlos",
-    "contraseña" : "carlos1122"
-}]
-
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
@@ -37,22 +36,47 @@ def index():
 def iniciar_sesion():
     if request.method == "POST":
         usuario = request.form.get("usuario")
-        registrar = request.form.get("registrarse")
-        for persona in administrador:
-            if persona.get("usuario") == usuario:
-                if persona.get("contraseña") == registrar:
-                    return "entraste como administrador"
-                
-        for persona in gp:
-            if persona.get("usuario") == usuario:
-                if persona.get("contraseña") == registrar:
-                    return "entraste como gp"
+        contraseña = request.form.get("contraseña")
+        for persona in administrador_base:
+            if persona.get("usuario") == usuario and persona.get("contraseña") == contraseña:
+                return "entraste como administrador"
+
+        for persona in gp_base:
+            if persona.get("usuario") == usuario and persona.get("contraseña") == contraseña:
+                return "entraste como gp"
+
+        for persona in empleados_base:
+            if persona.get("usuario") == usuario and persona.get("contraseña") == contraseña:
+                return "entraste como empleado"
 
     return render_template("iniciar_sesion.html")
 
+@app.route("/registrarse", methods=["GET", "POST"])
+def registrarse():
+    if request.method == "POST":
+        cedula = request.form.get("cedula")
+        nombre = request.form.get("nombre")
+        usuario = request.form.get("usuario_r")
+        contraseña = request.form.get("contraseña_r")
+        valor = request.form.get("valor")
 
-# with open(filename, "w", encoding="utf-8") as file:
-#     json.dump({"administrador" : administrador_base, "gestor de proyectos" : gp_base, "empleados" : empleados_base}, file, indent=5)
+        agregar = {
+            "cedula": cedula,
+            "nombre": nombre,
+            "usuario": usuario,
+            "contraseña": contraseña,
+        }
+
+        if valor == "empleado":
+            empleados_base.append(agregar)
+        elif valor == "gestor":
+            gp_base.append(agregar)
+
+        guardar_datos(administrador_base, gp_base, empleados_base)
+
+        return redirect("/iniciar_sesion")
+
+    return render_template("registrarse.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
